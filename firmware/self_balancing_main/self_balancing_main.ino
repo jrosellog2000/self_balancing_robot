e4f553cc86571b54d9934b07ee36567bf74aee64
jrosellog2000 <jrosellog2000@gmail.com>
2026-09-06 21:49:44 +0200

    Checking IMU sensor

--- firmware/self_balancing_main/self_balancing_main.ino
+++ firmware/self_balancing_main/self_balancing_main.ino
@@ -1,14 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
 
+MPU6050 sensor;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(8, 9);       
+  sensor.initialize();
+
+  if (sensor.testConnection()) Serial.println("Sensor iniciado correctamente");
+  else Serial.println("Error al iniciar el sensor");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  Serial.println("alive");
+  sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz);
+
+  Serial.print("a[x y z] g[x y z]:\t");
+  Serial.print(ax); Serial.print("\t");
+  Serial.print(ay); Serial.print("\t");
+  Serial.print(az); Serial.print("\t");
+  Serial.print(gx); Serial.print("\t");
+  Serial.print(gy); Serial.print("\t");
+  Serial.println(gz);
+
+  delay(100);
 }
