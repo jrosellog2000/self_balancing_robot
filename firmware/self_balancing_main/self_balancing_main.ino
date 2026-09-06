fc84f7389bdf9a9721bac04e6d41d0287cc10a97
jrosellog2000 <jrosellog2000@gmail.com>
2026-09-06 21:27:58 +0200

    Blinking LED test on ESP32

--- firmware/self_balancing_main/self_balancing_main.ino
+++ firmware/self_balancing_main/self_balancing_main.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  Serial.println("alive");
 }
